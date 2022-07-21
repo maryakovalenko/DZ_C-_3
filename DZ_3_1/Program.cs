a7d5e843e2cbf3a29f1786011fbe831b736f5f0e
maryakovalenko <pomashka18061992@gmail.com>
2022-07-21 19:09:08 +0300

    Program.cs

---
+++ DZ_3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Массивы и функции программирования
+
+Console.WriteLine("Задача №19");
+bool isPalindrome(int x) {
+    int div = 1;
+    while (x / div >= 10) {
+        div *= 10;
+    }
+    Console.WriteLine(div);
+    while (x != 0) {
+        int l = x / div;
+        int r = x % 10;
+        if (l != r) { 
+            Console.WriteLine("Not Palindrom");
+            return false;
+        }
+        x = (x % div) / 10;
+        div /= 100;
+    }
+    Console.WriteLine("Palindrom");
+    return true;
+}
+
+isPalindrome(14212);
+isPalindrome(12821);
+isPalindrome(23432);
